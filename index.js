529e9e4cfe92d567753ba32c7ac864ee1edb452b
Julian Kung <julianrkung@berkeley.edu>
2020-11-05 21:42:42 -0800

    Added ping endpoint for client to check if they are online or offline

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-
+import express from 'express';
+import cors from 'cors';
 import Airlock from 'airlock-server';
 
 const airlockPort = process.env.PORT || 4000;
-const apiKey = process.env.AIRTABLE_API_KEY
+const apiKey = process.env.AIRTABLE_API_KEY;
+
+const app = express();
+const port = process.env.PORT || 4000;
 
 new Airlock({
-    port: airlockPort,
+    // port: airlockPort,
+    server: app,
     airtableApiKey: [apiKey],
     airtableBaseId: process.env.AIRTABLE_BASE_ID,
     airtableUserTableName: 'Users',
@@ -13,3 +18,14 @@ new Airlock({
     airtablePasswordColumn: 'Password',
     allowedOrigins: ["http://localhost:3000"] // TODO: Change to be fetched from file
 });
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (_, res) => {
+    res.status(200);
+    res.json({ status: 'OK' })
+})
+
+app.listen(port, () => console.log(`Mee Panyar port ${port}!`));
