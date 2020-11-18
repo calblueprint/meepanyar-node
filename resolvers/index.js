2322381d910983bebd227b4a6e07ecfcc76e8ee5
Julian Kung <julianrkung@berkeley.edu>
2020-11-17 20:59:54 -0800

    Removed users resolver

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,8 +1,5 @@
 
 module.exports = {
-    Users: (userRecord, authRecord) => {
-        return userRecord.id === authRecord.id
-    },
     Sites: (siteRecord, authRecord) => {
         return siteRecord.fields.Users && siteRecord.fields.Users.includes(authRecord.id)
     }
