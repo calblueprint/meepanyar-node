232a71757046bd730807f88b5b1ad46a1b98fe33
Julian Kung <julianrkung@berkeley.edu>
2021-02-08 10:43:05 -0800

    Default origins

--- index.js
+++ index.js
@@ -3,21 +3,18 @@ import cors from 'cors';
 import Airlock from 'airlock-server';
 import { createCustomer, createManyMeterReadingsandInvoices, createManyPayments, createMeterReadingsandInvoice, createPayment } from './airtable/request';
 
-const airlockPort = process.env.PORT || 4000;
 const apiKey = process.env.AIRTABLE_API_KEY;
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 new Airlock({
-    // port: airlockPort,
     server: app,
     airtableApiKey: [apiKey],
     airtableBaseId: process.env.AIRTABLE_BASE_ID,
     airtableUserTableName: 'Users',
     airtableUsernameColumn: 'Username',
     airtablePasswordColumn: 'Password',
-    allowedOrigins: ["http://localhost:3000", "http://localhost:5000"] // TODO: Change to be fetched from file
 });
 
 app.use(cors());
