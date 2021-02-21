6b7c25c1aa9e0fee5e8f74337eba8e4aa2d20348
Annie Wang <anniedwang02@gmail.com>
2021-02-21 12:28:12 -0800

    Pull tariff plan data

--- airtable/utils.js
+++ airtable/utils.js
@@ -1,19 +1,30 @@
-
 // Function receives a list of customers, meterReadings, and payments,
-// and adds the appropriate meter readings and payments to each customer object
-export const matchCustomersWithReadingsAndPayments = (customers, meterReadings, payments) => {
-    customers.forEach(customer => {
-        const customerId = customer.id;
-        customer.meterReadings = [];
-        customer.payments = [];
+// and adds the appropriate meter readings, payments, and tariff info to each customer object
+export const matchCustomersWithReadingsPaymentsAndTariff = (
+  customers,
+  meterReadings,
+  payments,
+  tariffPlans
+) => {
+  customers.forEach((customer) => {
+    const customerId = customer.id;
+    customer.meterReadings = [];
+    customer.payments = [];
 
-        const customerMeterReadings =
-            meterReadings.filter(meterReading => meterReading.customerId === customerId);
+    const customerMeterReadings = meterReadings.filter(
+      (meterReading) => meterReading.customerId === customerId
+    );
 
-        const customerPayments =
-            payments.filter(payment => payment.customerId === customerId);
+    const customerPayments = payments.filter(
+      (payment) => payment.customerId === customerId
+    );
 
-        customer.meterReadings = customerMeterReadings;
-        customer.payments = customerPayments;
-    })
-}
+    const customerTariff = tariffPlans.filter((tariff) =>
+      tariff.customerIds.includes(customerId)
+    );
+
+    [customer.tariffPlans] = customerTariff;
+    customer.meterReadings = customerMeterReadings;
+    customer.payments = customerPayments;
+  });
+};
--- resolvers/index.js
+++ resolvers/index.js
@@ -1,40 +1,69 @@
-const { getCustomersByIds, getMeterReadingsandInvoicesByIds, getPaymentsByIds } = require("../airtable/request");
-const { matchCustomersWithReadingsAndPayments } = require("../airtable/utils");
+const {
+  getCustomersByIds,
+  getMeterReadingsandInvoicesByIds,
+  getPaymentsByIds,
+  getTariffPlanById,
+  getTariffPlansByIds,
+} = require("../airtable/request");
+const {
+  matchCustomersWithReadingsAndPayments,
+  matchCustomersWithReadingsPaymentsAndTariff,
+} = require("../airtable/utils");
 
 module.exports = {
-    Sites: async (siteRecord, authRecord) => {
-        if (!siteRecord.fields.Users || !siteRecord.fields.Users.includes(authRecord.id)) {
-            return false;
-        }
-
-        // We resolve each site's customerId to their appropriate customer
-        const customerIds = siteRecord.fields.Customers;
-        if (customerIds) {
-            const customers = await getCustomersByIds(customerIds);
-            const meterReadingIds = [];
-            const paymentIds = [];
-
-            // We get all the meter reading and payment ids for all customers
-            // to minimize Airtable API calls, and then match the meter readings
-            // and payments received to their appropriate customer afterwards
-            customers.forEach(customer => {
-                const customerMeterReadingIds = customer.meterReadingIds;
-                const customerPaymentIds = customer.paymentIds;
-
-                if (customerMeterReadingIds) {
-                    meterReadingIds.push(...customerMeterReadingIds);
-                }
-
-                if (customerPaymentIds) {
-                    paymentIds.push(...customerPaymentIds);
-                }
-            })
-            const meterReadings = await getMeterReadingsandInvoicesByIds(meterReadingIds);
-            const payments = await getPaymentsByIds(paymentIds);
-            matchCustomersWithReadingsAndPayments(customers, meterReadings, payments);
-            siteRecord.fields.Customers = customers;
-        }
-
-        return siteRecord;
+  Sites: async (siteRecord, authRecord) => {
+    if (
+      !siteRecord.fields.Users ||
+      !siteRecord.fields.Users.includes(authRecord.id)
+    ) {
+      return false;
     }
-}
+
+    // We resolve each site's customerId to their appropriate customer
+    const customerIds = siteRecord.fields.Customers;
+    if (customerIds) {
+      const customers = await getCustomersByIds(customerIds);
+      const meterReadingIds = [];
+      const paymentIds = [];
+      const tariffPlanIds = [];
+
+      // We get all the meter reading and payment ids for all customers
+      // to minimize Airtable API calls, and then match the meter readings
+      // and payments received to their appropriate customer afterwards
+      customers.forEach((customer) => {
+        const customerMeterReadingIds = customer.meterReadingIds;
+        const customerPaymentIds = customer.paymentIds;
+        const customerTariffPlansIds = customer.tariffPlansId;
+
+        if (customerMeterReadingIds) {
+          meterReadingIds.push(...customerMeterReadingIds);
+        }
+
+        if (customerPaymentIds) {
+          paymentIds.push(...customerPaymentIds);
+        }
+
+        if (customerTariffPlansIds) {
+          tariffPlanIds.push(customerTariffPlansIds);
+        }
+      });
+
+      let [meterReadings, payments, tariffPlans] = await Promise.all([
+        getMeterReadingsandInvoicesByIds(meterReadingIds),
+        getPaymentsByIds(paymentIds),
+        getTariffPlansByIds(tariffPlanIds),
+      ]);
+
+      matchCustomersWithReadingsPaymentsAndTariff(
+        customers,
+        meterReadings,
+        payments,
+        tariffPlans
+      );
+
+      siteRecord.fields.Customers = customers;
+    }
+
+    return siteRecord;
+  },
+};
