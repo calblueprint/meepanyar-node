3eca3f861a03f4fde95c60c55fe120b178d11a72
Annie Wang <wangannie@berkeley.edu>
2021-03-03 20:19:48 -0800

    Add customer tariff plan to site pull (#6)
    * Pull tariff plan data
    
    * Rename matchCustomers
    
    * Switch to pull all tariffs for current site
    
    * Condense all awaits

--- airtable/utils.js
+++ airtable/utils.js
@@ -1,19 +1,20 @@
-
 // Function receives a list of customers, meterReadings, and payments,
 // and adds the appropriate meter readings and payments to each customer object
-export const matchCustomersWithReadingsAndPayments = (customers, meterReadings, payments) => {
-    customers.forEach(customer => {
-        const customerId = customer.id;
-        customer.meterReadings = [];
-        customer.payments = [];
+export const matchCustomers = (customers, meterReadings, payments) => {
+  customers.forEach((customer) => {
+    const customerId = customer.id;
+    customer.meterReadings = [];
+    customer.payments = [];
 
-        const customerMeterReadings =
-            meterReadings.filter(meterReading => meterReading.customerId === customerId);
+    const customerMeterReadings = meterReadings.filter(
+      (meterReading) => meterReading.customerId === customerId
+    );
 
-        const customerPayments =
-            payments.filter(payment => payment.customerId === customerId);
+    const customerPayments = payments.filter(
+      (payment) => payment.customerId === customerId
+    );
 
-        customer.meterReadings = customerMeterReadings;
-        customer.payments = customerPayments;
-    })
-}
+    customer.meterReadings = customerMeterReadings;
+    customer.payments = customerPayments;
+  });
+};
--- resolvers/index.js
+++ resolvers/index.js
@@ -1,46 +1,85 @@
-const { getCustomersByIds, getMeterReadingsandInvoicesByIds, getPaymentsByIds, getFinancialSummariesByIds } = require("../airtable/request");
-const { matchCustomersWithReadingsAndPayments } = require("../airtable/utils");
+const {
+  getCustomersByIds,
+  getMeterReadingsandInvoicesByIds,
+  getPaymentsByIds,
+  getFinancialSummariesByIds,
+  getTariffPlansByIds,
+} = require("../airtable/request");
+const { matchCustomers } = require("../airtable/utils");
 
 module.exports = {
-    Sites: async (siteRecord, authRecord) => {
-        if (!siteRecord.fields.Users || !siteRecord.fields.Users.includes(authRecord.id)) {
-            return false;
-        }
-
-        // We resolve each site's customerId to their appropriate customer
-        const customerIds = siteRecord.fields.Customers;
-        if (customerIds) {
-            const customers = await getCustomersByIds(customerIds);
-            const meterReadingIds = [];
-            const paymentIds = [];
-
-            // We get all the meter reading and payment ids for all customers
-            // to minimize Airtable API calls, and then match the meter readings
-            // and payments received to their appropriate customer afterwards
-            customers.forEach(customer => {
-                const customerMeterReadingIds = customer.meterReadingIds;
-                const customerPaymentIds = customer.paymentIds;
-
-                if (customerMeterReadingIds) {
-                    meterReadingIds.push(...customerMeterReadingIds);
-                }
-
-                if (customerPaymentIds) {
-                    paymentIds.push(...customerPaymentIds);
-                }
-            })
-            const meterReadings = await getMeterReadingsandInvoicesByIds(meterReadingIds);
-            const payments = await getPaymentsByIds(paymentIds);
-            matchCustomersWithReadingsAndPayments(customers, meterReadings, payments);
-            siteRecord.fields.Customers = customers;
-        }
-
-        const financialSummaryIds = siteRecord.fields["Financial Summaries"];
-        let financialSummaries = [];
-        if (financialSummaryIds) {
-            financialSummaries = await getFinancialSummariesByIds(financialSummaryIds);
-        }
-        siteRecord.fields.FinancialSummaries = financialSummaries;
-        return siteRecord;
+  Sites: async (siteRecord, authRecord) => {
+    if (
+      !siteRecord.fields.Users ||
+      !siteRecord.fields.Users.includes(authRecord.id)
+    ) {
+      return false;
     }
-}
+
+    const promises = [];
+    // We resolve each site's customerId to their appropriate customer
+    const customerIds = siteRecord.fields.Customers;
+    let customers = [];
+    let meterReadings = [];
+    let payments = [];
+    if (customerIds) {
+      customers = await getCustomersByIds(customerIds);
+      const meterReadingIds = [];
+      const paymentIds = [];
+
+      // We get all the meter reading and payment ids for all customers
+      // to minimize Airtable API calls, and then match the meter readings
+      // and payments received to their appropriate customer afterwards
+      customers.forEach((customer) => {
+        const customerMeterReadingIds = customer.meterReadingIds;
+        const customerPaymentIds = customer.paymentIds;
+
+        if (customerMeterReadingIds) {
+          meterReadingIds.push(...customerMeterReadingIds);
+        }
+
+        if (customerPaymentIds) {
+          paymentIds.push(...customerPaymentIds);
+        }
+      });
+
+      const meterReadingsPromise = getMeterReadingsandInvoicesByIds(
+        meterReadingIds
+      ).then((res) => (meterReadings = res));
+      promises.push(meterReadingsPromise);
+
+      const paymentsPromise = getPaymentsByIds(paymentIds).then(
+        (res) => (payments = res)
+      );
+      promises.push(paymentsPromise);
+    }
+
+    const financialSummaryIds = siteRecord.fields["Financial Summaries"];
+    let financialSummaries = [];
+    if (financialSummaryIds) {
+      const financialSummariesPromise = getFinancialSummariesByIds(
+        financialSummaryIds
+      ).then((res) => (financialSummaries = res));
+      promises.push(financialSummariesPromise);
+    }
+
+    const tariffPlanIds = siteRecord.fields["Tariff Plans"];
+    let tariffPlans = [];
+    if (tariffPlanIds) {
+      const tariffPlansPromise = getTariffPlansByIds(tariffPlanIds).then(
+        (res) => (tariffPlans = res)
+      );
+      promises.push(tariffPlansPromise);
+    }
+
+    // Await all promises at once
+    await Promise.all(promises);
+
+    matchCustomers(customers, meterReadings, payments);
+    siteRecord.fields.Customers = customers;
+    siteRecord.fields.FinancialSummaries = financialSummaries;
+    siteRecord.fields.TariffPlans = tariffPlans;
+
+    return siteRecord;
+  },
+};
