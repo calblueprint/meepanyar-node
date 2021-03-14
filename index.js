c6985ef90b3cf47cbc8143fef90d1c245d7981a8
Annie Wang <anniedwang02@gmail.com>
2021-03-13 16:54:10 -0800

    Add inventory/create and inventory-update/create

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-import express from 'express';
-import cors from 'cors';
-import Airlock from 'airlock-server';
-import { createCustomer, createManyMeterReadingsandInvoices, createManyPayments, createMeterReadingsandInvoice, createPayment } from './airtable/request';
+import Airlock from "airlock-server";
+import cors from "cors";
+import express from "express";
+import {
+  createCustomer,
+  createInventory,
+  createInventoryUpdate,
+  createManyMeterReadingsandInvoices,
+  createManyPayments,
+  createMeterReadingsandInvoice,
+  createPayment,
+} from "./airtable/request";
 
 const airlockPort = process.env.PORT || 4000;
 const apiKey = process.env.AIRTABLE_API_KEY;
@@ -10,111 +18,165 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 new Airlock({
-    // port: airlockPort,
-    server: app,
-    airtableApiKey: [apiKey],
-    airtableBaseId: process.env.AIRTABLE_BASE_ID,
-    airtableUserTableName: 'Users',
-    airtableUsernameColumn: 'Username',
-    airtablePasswordColumn: 'Password',
-    allowedOrigins: ["http://localhost:3000", "http://localhost:5000"] // TODO: Change to be fetched from file
+  // port: airlockPort,
+  server: app,
+  airtableApiKey: [apiKey],
+  airtableBaseId: process.env.AIRTABLE_BASE_ID,
+  airtableUserTableName: "Users",
+  airtableUsernameColumn: "Username",
+  airtablePasswordColumn: "Password",
+  allowedOrigins: ["http://localhost:3000", "http://localhost:5000"], // TODO: Change to be fetched from file
 });
 
 app.use(cors());
 app.use(express.json());
 
-
-app.get('/', (_, result) => {
-    result.status(200);
-    result.json({ status: 'OK' })
-})
+app.get("/", (_, result) => {
+  result.status(200);
+  result.json({ status: "OK" });
+});
 
 // This endpoint is used to create customers from the frontend.
 // To support offline functionality while keeping our API stateless,
 // we add meterReadings and payments for offline customers into the
 // customerData payload, and create meterReadings and payments for
 // the customer created after the customer has been created in airtable.
-app.post('/customers/create', async (request, result) => {
-    const customerData = request.body;
-    console.log("Customer Creation Payload: ", customerData);
+app.post("/customers/create", async (request, result) => {
+  const customerData = request.body;
+  console.log("Customer Creation Payload: ", customerData);
 
-    try {
-        const { name, meterNumber, tariffPlanId, siteId, meterReadings, payments } = customerData;
-        const hasMeter = meterNumber ? true : false;
-        const isActive = true;
+  try {
+    const {
+      name,
+      meterNumber,
+      tariffPlanId,
+      siteId,
+      meterReadings,
+      payments,
+    } = customerData;
+    const hasMeter = meterNumber ? true : false;
+    const isActive = true;
 
-        const airtableCustomerData = {
-            isactive: isActive,
-            hasmeter: hasMeter,
-            tariffPlanId,
-            siteId,
-            name,
-            meterNumber
-        };
+    const airtableCustomerData = {
+      isactive: isActive,
+      hasmeter: hasMeter,
+      tariffPlanId,
+      siteId,
+      name,
+      meterNumber,
+    };
 
-        const customerId = await createCustomer(airtableCustomerData);
-        console.log("Customer id: ", customerId);
-        console.log("Customer created!");
+    const customerId = await createCustomer(airtableCustomerData);
+    console.log("Customer id: ", customerId);
+    console.log("Customer created!");
 
-        if (meterReadings) {
-            try {
-                meterReadings.forEach(meterReading => meterReading.customerId = customerId);
-                createManyMeterReadingsandInvoices(meterReadings);
-                console.log("Created meter readings")
-            } catch (err) {
-                console.log("Meter reading error: ", err);
-            }
-        }
-
-        if (payments) {
-            try {
-                payments.forEach(payment => payment.customerId = customerId);
-                createManyPayments(payments)
-                console.log("Created payments")
-            } catch (err) {
-                console.log("Payments error: ", err);
-            }
-        }
-
-        result.status(201);
-        result.json({ status: 'OK' })
-    } catch (err) {
-        console.log(err);
-        result.status(400);
-        result.json({ error: err });
+    if (meterReadings) {
+      try {
+        meterReadings.forEach(
+          (meterReading) => (meterReading.customerId = customerId)
+        );
+        createManyMeterReadingsandInvoices(meterReadings);
+        console.log("Created meter readings");
+      } catch (err) {
+        console.log("Meter reading error: ", err);
+      }
     }
-})
+
+    if (payments) {
+      try {
+        payments.forEach((payment) => (payment.customerId = customerId));
+        createManyPayments(payments);
+        console.log("Created payments");
+      } catch (err) {
+        console.log("Payments error: ", err);
+      }
+    }
+
+    result.status(201);
+    result.json({ status: "OK" });
+  } catch (err) {
+    console.log(err);
+    result.status(400);
+    result.json({ error: err });
+  }
+});
 
 // Endpoint used to create meter readings when the meter reading
 // contains a customer id (the customer already exists in airtable).
-app.post('/meter-readings-and-invoices/create', async (request, result) => {
-    try {
-        const meterReadingData = request.body;
-        console.log("Meter reading data: ", meterReadingData);
-        const resultData = await createMeterReadingsandInvoice(meterReadingData);
-        result.status(201);
-        result.json({ status: 'OK', id: resultData });
-    } catch (err) {
-        console.log('Error when creating meter reading and invoice: ', err);
-        result.status(400);
-        result.json({ error: err });
-    }
-})
+app.post("/meter-readings-and-invoices/create", async (request, result) => {
+  try {
+    const meterReadingData = request.body;
+    console.log("Meter reading data: ", meterReadingData);
+    const resultData = await createMeterReadingsandInvoice(meterReadingData);
+    result.status(201);
+    result.json({ status: "OK", id: resultData });
+  } catch (err) {
+    console.log("Error when creating meter reading and invoice: ", err);
+    result.status(400);
+    result.json({ error: err });
+  }
+});
 
 // Endpoint used to create payments when the payment
 // contains a customer id (the customer already exists in airtable).
-app.post('/payments/create', async (request, result) => {
-    try {
-        const paymentData = request.body;
-        console.log("Payment data: ", paymentData);
-        const resultData = await createPayment(paymentData);
-        result.status(201);
-        result.json({ status: 'OK', id: resultData });
-    } catch (err) {
-        console.log('Error when creating payment: ', err);
-        result.status(400);
-        result.json({ error: err });
-    }
-})
+app.post("/payments/create", async (request, result) => {
+  try {
+    const paymentData = request.body;
+    console.log("Payment data: ", paymentData);
+    const resultData = await createPayment(paymentData);
+    result.status(201);
+    result.json({ status: "OK", id: resultData });
+  } catch (err) {
+    console.log("Error when creating payment: ", err);
+    result.status(400);
+    result.json({ error: err });
+  }
+});
+
+// Endpoint used to create a new inventory item.
+// Also logs the update in an InventoryUpdate.
+// contains a site id (the site already exists in airtable).
+app.post("/inventory/create", async (request, result) => {
+  try {
+    const inventoryData = request.body;
+    // Remove the blank id field
+    delete inventoryData.id;
+    console.log("Inventory data: ", inventoryData);
+
+    const inventoryId = await createInventory(inventoryData);
+    console.log("Inventory id:", inventoryId);
+    console.log("Inventory created!");
+
+    // TODO: add creation of inventory update
+    result.status(201);
+    result.json({ status: "OK", iid: inventoryId });
+  } catch (err) {
+    console.log("Error when creating inventory: ", err);
+    result.status(400);
+    result.json({ error: err });
+  }
+});
+
+// Endpoint used to create a new inventory update.
+// contains a site id (the site already exists in airtable).
+app.post("/inventory-update/create", async (request, result) => {
+  try {
+    const updateData = request.body;
+    // Remove the blank id field
+    delete updateData.id;
+    console.log("Inventory Update data: ", updateData);
+
+    const inventoryUpdateId = await createInventoryUpdate(updateData);
+    console.log("Inventory Update id:", inventoryId);
+    console.log("Inventory Update created!");
+
+    result.status(201);
+    result.json({ status: "OK", id: inventoryUpdateId });
+  } catch (err) {
+    console.log("Error when creating inventory update: ", err);
+    result.status(400);
+    result.json({ error: err });
+  }
+});
 
 app.listen(port, () => console.log(`Mee Panyar port ${port}!`));
